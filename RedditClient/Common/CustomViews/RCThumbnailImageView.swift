301597d161764757abf3ad8d9867d9e1ba7ef0fe
Fernando Luna <337n4nd0@gmail.com>
2020-10-05 17:41:20 -0400

    PostList View Implementation

---
+++ RedditClient/Common/CustomViews/RCThumbnailImageView.swift
@@ -0,0 +1,20 @@
+//
+//  RCThumbnailImageView.swift
+//  RedditClient
+//
+//  Created by Fernando Luna on 10/5/20.
+//
+
+import UIKit
+
+class RCThumbnailImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
